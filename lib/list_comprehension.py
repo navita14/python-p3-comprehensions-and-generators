eaaf10995ca11df9ab237abf9d5781cf836e7fe8
Navita Ramasar <navitaramasar14@gmail.com>
2023-10-20 20:03:04 -0400

    completed lab on my own

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
 def return_evens(num_list):
-    new_arr=[]
-      #  new_arr=[x for x in num_list if x % 2 == 0]
-        #return new_arr
+    nums = []
     for x in num_list:
         if x % 2 == 0:
-            new_arr.append(x)
-    return new_arr
+            nums.append(x)
+    return nums
+
     
 
 def make_exclamation(sentence_list):
-    # new_sentence_list = [sentence + '!' for sentence in sentence_list]
-    # return new_sentence_list
-    new_sentence_list = []
+    new_list = []
     for sentence in sentence_list:
-        new_sentence = sentence + "!"
-        new_sentence_list.append(new_sentence)
-    return new_sentence_list
+        new_list.append(sentence + "!")
+    return new_list
+    
+            
